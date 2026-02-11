64e1b0c3e52fe359be70e05be484c5d92fcd8ce4
Roberto Avalos <roberto.avalos@ikontechnologies.com>
2026-02-10 22:21:53 -0600

    chore(release): bump version to 2026.02.10.02

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,5 +10,5 @@ export const environment = {
   user: '',
   pass: '',
   theme: 'modern',
-  appVersion: '2026.02.10.01',
+  appVersion: '2026.02.10.02',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -30,7 +30,7 @@ export const environment = {
   user: 'admin@admin.com',
   pass: 'admin',
   theme: 'ocean',
-  appVersion: '2026.02.10.01',
+  appVersion: '2026.02.10.02',
 };
 
 /*
